b9ae18f47cb916ac4d6dbead8aff69343fb87da2
mrarbi <mouaad.arbi@gmail.com>
2014-01-14 11:19:52 +0000

    Update HelloWorldAction.java

--- appweb/src/main/java/org/test/struts/appweb/HelloWorldAction.java
+++ appweb/src/main/java/org/test/struts/appweb/HelloWorldAction.java
@@ -1,7 +1,5 @@
 package org.test.struts.appweb;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 
 /**
  * 
@@ -10,16 +8,9 @@ import org.apache.log4j.Logger;
  */
 public class HelloWorldAction {
 
-	Logger logger = LogManager.getLogger(HelloWorldAction.class.getName());
-
 	private String name;
 
 	public String execute() throws Exception {
-		try {
-			throw new Exception("test for exception");
-		} catch (Exception e) {
-			logger.error(e);
-		}
 		return "success";
 	}
 
